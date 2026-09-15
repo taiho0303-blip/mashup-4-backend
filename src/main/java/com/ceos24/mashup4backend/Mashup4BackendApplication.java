e468a81288c43f15913923955d7b90558e750fff
taiho0303 <taiho0303@gmail.com>
2026-09-15 20:48:13 +0900

    feat: initialize Spring Boot project

---
+++ src/main/java/com/ceos24/mashup4backend/Mashup4BackendApplication.java
@@ -0,0 +1,13 @@
+package com.ceos24.mashup4backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mashup4BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mashup4BackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ceos24/mashup4backend/Mashup4BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ceos24.mashup4backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mashup4BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
